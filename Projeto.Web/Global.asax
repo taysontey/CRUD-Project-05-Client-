37086ce35f35c4c7418e21af7c0989da2fe8d957
taysontey <taysontey@gmail.com>
2016-04-01 11:58:56 -0300

    Cadastrar/Consultar(Time)(1)

---
+++ Projeto.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Projeto.Web.WebApiApplication" Language="C#" %>
---
+++ Projeto.Web/app/app.js
@@ -0,0 +1,46 @@
+﻿var app = angular.module('app', ['ngRoute']);
+
+app.config(
+    function ($routeProvider) {
+
+        $routeProvider
+            .when(
+                '/cadastrarTime',
+                {
+                    templateUrl: 'app/views/time/cadastro.html',
+                    controller: 'timeCtrl'
+                }
+            )
+            .when(
+                '/consultarTime',
+                {
+                    templateUrl: 'app/views/time/consulta.html',
+                    controller: 'timeCtrl'
+                }
+            );
+    }
+);
+
+app.controller('timeCtrl', function ($scope, $http) {
+
+    $scope.msg = "";
+
+    $scope.cadastrar = function (time) {
+        $http.post("http://localhost:49199/services/time/cadastrar", time)
+        .success(function (msg) {
+            $scope.msg = msg;
+        })
+        .error(function (e) {
+            $scope.msg = e;
+        });
+    };
+
+    $http.get("http://localhost:49199/services/time/consultar")
+    .success(function (lista) {
+        $scope.times = lista;
+    })
+    .error(function (e) {
+        $scope.msg = e;
+    });
+
+});
